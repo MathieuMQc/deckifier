86da21f4357d035af8a7a69daa1bc4dc1a8844f4
TGRush <simeonlps@protonmail.com>
2022-04-16 23:51:30 +0200

    :trollface:
    The changes made are mostly shellcheck related -_-

--- install.sh
+++ install.sh
@@ -19,18 +19,18 @@ reset="\e[0m"
 deckifierinstall () {
     echo -e " ${green}Installing deckifier, hang tight!"
     # read -p "Please make sure that you have multilib repository enabled, yay and git installed, then press enter to continue."
-    
+    read -p -r "Please make sure the Multilib repository is enabled, otherwise, the installation will fail!"
     sudo pacman -Sy steam gamescope jq dmidecode
     echo "Steam is installed. Running it for checking updates."
-    read -p "Once it's done, close it and don't log in. After closing, press enter to continue."
+    read -p -r "Once it's done, close it and don't log in. After closing, press enter to continue."
     steam
-    read -p "Steam update finished, close it and press enter to continue."
+    read -p -r "Steam update finished, close it and press enter to continue."
     echo "Installing Mangohud and Steam Deck files."
     yay -S mangohud
     # shortened this to what it is below
     # sudo cp -r etc /
     # sudo cp -r usr /
-    sudo cp -r {etc, usr} /
+    sudo cp -r "{etc, usr}" "/"
     sudo chmod 0644 /usr/share/polkit-1/actions/org.val*
     # shortened this too
     # sudo chmod +x /usr/bin/jupiter*
@@ -38,7 +38,7 @@ deckifierinstall () {
     # sudo chmod +x /usr/bin/mangoapp
     # sudo chmod +x /usr/bin/gamescope-session
     # sudo chmod +x /usr/bin/steamos-polkit-helpers/*
-    sudo chmod +x /usr/bin/{jupiter*, steamos*, mangoapp*, gamescope-session*, steamos-polkit-helpers*}
+    sudo chmod +x "/usr/bin/{jupiter*, steamos*, mangoapp*, gamescope-session*, steamos-polkit-helpers/*}"
     sudo pacman -S glew glfw-wayland 
     echo -e "${green}Installation complete, now you can run the following or select the \"SteamOS\" session in your Display Manager"
     echo -e "${reset}Command to run Steam Deck UI: steam -steamos3 -steampal -steamdeck -gamepadui"
@@ -46,6 +46,7 @@ deckifierinstall () {
 
 # check if you're even running Arch Linux (with yay) in the first place by checking if pacman and yay available
 archcheck () {
+    #shellcheck disable=SC1014,SC2050,SC2057
     if [ command -v pacman ] && [ command -v yay ]; then
         deckifierinstall
     else
--- readme.md
+++ readme.md
@@ -2,24 +2,26 @@
 
 ***SteamOS session on any Arch-based distro!***
 
-This repository aims to add required SteamDeck's holo packages and binaries such as `steamos-update, jupiter-biosupdate, steamosatomupd`, Gamescope Wayland session and etc.
+This repository aims to add required SteamDeck's holo packages and binaries such as `steamos-update, jupiter-biosupdate, steamosatomupd`, Gamescope Wayland session and other required components to Arch Linux
 
-This adds all of the required SteamOS dependencies, so the things such as shader pre-caching downloading, FPS;TDP;GPU clock limiting, flyouts, update daemon, performance overlay works as intended!
+This adds all of the required SteamOS dependencies, as well as everything needed for shader pre-cache downloading, FPS;TDP;GPU clock limiting, flyouts, update daemon, performance overlay and so on
 
 Bugs: 
 
 - Battery status doesn't work
 - global FSR doesn't work (normal due to mesa/linux doesn't support that by default)
 
+<!-- old readme bugs list -->
 <!-- currently i don't know how to enable battery status for laptops, global FSR doesn't work (intended behavior, since extra/mesa and core/linux kernel doesn't support that. You may install SteamOS3 mesa and linux-neptune later if you want to try it) -->
 
 Installation:
-Before installing, make sure multilib repository is enabled in /etc/pacman.conf, yay and git installed.
+Before installing, make sure the `multilib` repository is enabled in /etc/pacman.conf and that you have yay installed.
 the installation will fail otherwise!
 
-Open terminal, clone this repository, go inside this directory and type:
+Open a Terminal and do the following:
 ```
+git clone https://github.com/bhaiest/deckifier.git
+cd deckifier
 chmod +x install.sh
 ./install.sh
 ```
- 
