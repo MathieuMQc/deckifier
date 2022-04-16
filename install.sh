5b0b5e4ee14384e2a2ed414d814963d6ee4295fa
Adam Jafarov <bhaiest@pqenablers.cf>
2022-04-16 18:50:28 +0300

    add polkit rules and install correct glfw

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Installing deckifier, hang tight!"
 read -p "Please make sure that you have multilib repository enabled, yay and git installed, then press enter to continue."
-sudo pacman -Sy steam gamescope jq
+sudo pacman -Sy steam gamescope jq dmidecode
 echo "Steam is installed. Running it for checking updates."
 read -p "Once it's done, close it and don't log in. After closing, press enter to continue."
 steam
@@ -10,10 +10,11 @@ echo "Installing Mangohud and Steam Deck files."
 yay -S mangohud
 sudo cp -r etc /
 sudo cp -r usr /
+sudo chmod 0644 /usr/share/polkit-1/actions/org.val*
 sudo chmod +x /usr/bin/jupiter*
 sudo chmod +x /usr/bin/steamos*
 sudo chmod +x /usr/bin/mangoapp
 sudo chmod +x /usr/bin/gamescope-session
 sudo chmod +x /usr/bin/steamos-polkit-helpers/*
-sudo pacman -S glew glfw
+sudo pacman -S glew glfw-wayland
 read -p "Installation complete, now you can run steam -steamos3 -steampal -steamdeck -gamepadui or logout to your display manager and select SteamOS Session"
